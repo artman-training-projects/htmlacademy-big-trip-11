6fe30c5f0f67533278ab18719c6c4de43b96ee75
Art <ya.Artriches@ya.ru>
2020-04-22 17:41:54 +0300

    fix

--- src/controllers/tripController.js
+++ src/controllers/tripController.js
@@ -1,31 +1,81 @@
 import {sortEvents} from '../utils/sorting';
-import {renderTrip} from '../utils/renderTrip';
-import {RenderPosition, renderComponent} from '../utils/element';
+import {RenderPosition, renderComponent, replaceComponent} from '../utils/element';
 
-
-import MainEventNo from '../components/page-main/event-no';
+import NoEvents from '../components/no-events';
 import HeaderTripInfo from '../components/page-header/trip-info';
 import HeaderTripMenu from '../components/page-header/trip-menu';
 import HeaderTripFilter from '../components/page-header/trip-filter';
 import MainEventsSort from '../components/page-main/trip-sort';
+import MainTripDay from '../components/page-main/trip-day/trip-days__item';
 import MainTripDays from '../components/page-main/trip-days';
+import MainTripDayEvent from '../components/page-main/trip-day/trip-events__item';
+import MainTripDayEventEdit from '../components/page-main/event-edit';
+
+const renderTripEvent = (event, day) => {
+  const replaceEventToEdit = () => {
+    replaceComponent(tripEventEdit, tripEvent);
+    document.addEventListener(`keydown`, onEscKeyDown);
+  };
+
+  const replaceEditToEvent = () => {
+    replaceComponent(tripEvent, tripEventEdit);
+    document.removeEventListener(`keydown`, onEscKeyDown);
+  };
+
+  const onEscKeyDown = (evt) => {
+    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+
+    if (isEscKey) {
+      replaceEditToEvent();
+    }
+  };
+
+  const tripEvent = new MainTripDayEvent(event);
+  tripEvent.setButtonRollupClickHandler(() => {
+    replaceEventToEdit();
+  });
+
+  const tripEventEdit = new MainTripDayEventEdit(event);
+  tripEventEdit.setButtonEventSaveClick(() => {
+    replaceEditToEvent();
+  });
+  tripEventEdit.setButtonEventResetClick(() => {
+    replaceEditToEvent();
+  });
+
+  renderComponent(day, tripEvent);
+};
+
+const renderTrip = (events, tripDays) => {
+
+  let dayFrom;
+  let daysCount = 0;
+  let tripDay;
+
+  for (const event of events) {
+    if (dayFrom !== event.dateFrom.getDate()) {
+      dayFrom = event.dateFrom.getDate();
+      daysCount++;
+      dayFrom = event.dateFrom.getDate();
+      const dayTrip = new MainTripDay(daysCount, event);
+      tripDay = dayTrip.getElement().querySelector(`.trip-events__list`);
+      renderComponent(tripDays, dayTrip);
+    }
+
+    renderTripEvent(event, tripDay);
+  }
+};
 
 export default class TripController {
   constructor(container) {
+    this._noEvents = new NoEvents();
+    this._mainEventSort = new MainEventsSort();
     this._container = container;
   }
 
   render(trip) {
     if (!trip) {
-      const tripMain = document.querySelector(`.trip-main`);
-      renderComponent(tripMain, new HeaderTripInfo(trip), RenderPosition.AFTERBEGIN);
-
-      const tripControls = tripMain.querySelector(`.trip-controls`);
-      renderComponent(tripControls, new HeaderTripMenu());
-      renderComponent(tripControls, new HeaderTripFilter());
-
-      const tripEvents = document.querySelector(`.trip-events`);
-      renderComponent(tripEvents, new MainEventNo());
+      this._noEvents.getTemplate();
     } else {
       const tripsSortedByDateFrom = sortEvents(trip);
       // console.log(JSON.stringify(trips[0], null, 2));
